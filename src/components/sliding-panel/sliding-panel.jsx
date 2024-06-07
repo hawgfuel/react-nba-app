57dc12a27ade618f12c8fc83aaeff036d40bdea5
Alan Werstler <hawgfuel@gmail.com>
2024-06-07 12:28:28 -0700

    adding grey basketball for misses

--- src/components/sliding-panel/sliding-panel.jsx
+++ src/components/sliding-panel/sliding-panel.jsx
@@ -4,7 +4,8 @@ import { getData } from '../../clients/getData';
 import Dropdown from '../../components/dropdown/dropdown';
 import './sliding-panel.css';
 import court from '../../images/nbahalfcourt.png';
-import marker from '../../images/basketball.png';  // Add your marker image here
+import markerGreen from '../../images/basketball.png';  // Green marker image
+import markerRed from '../../images/basketball-grey.png';  // Red marker image
 
 const SlidingPanel = ({ yearArr }) => {
   SlidingPanel.propTypes = {
@@ -17,11 +18,12 @@ const SlidingPanel = ({ yearArr }) => {
   const [filteredCoordinates, setFilteredCoordinates] = useState([]);
   const [uniqueDates, setUniqueDates] = useState([]);
   const [gameDate, setGameDate] = useState(null);
+  const [totalShots, setTotalShots] = useState(0);
+  const [totalMadeShots, setTotalMadeShots] = useState(0);
   const canvasRef = useRef(null);
   const imageRef = useRef(null);
-  const markerRef = useRef(null);  // Add reference for the marker image
   const baseUrl = 'https://nba-stats-db.herokuapp.com/api/shot_chart_data/';
-  const playerArr = ['Lebron James', 'James Harden', 'Stephen Curry']
+  const playerArr = ['Lebron James', 'James Harden', 'Stephen Curry'];
   const [playerName, setPlayerName] = useState(playerArr[0]);
   const [urlYear, setUrlYear] = useState(yearArr[yearArr.length - 1]);
   const togglePanel = () => {
@@ -57,6 +59,16 @@ const SlidingPanel = ({ yearArr }) => {
     if (gameDate) {
       const filtered = coordinates.filter(coord => coord.date === gameDate);
       setFilteredCoordinates(filtered);
+
+      // Update total shot counts
+      setTotalShots(filtered.length);
+
+      // Update total made shot counts (green shots)
+      const totalMade = filtered.reduce((acc, curr) => {
+        return curr.color === 'green' ? acc + 1 : acc;
+      }, 0);
+      setTotalMadeShots(totalMade);
+      setFilteredCoordinates(filtered);
     }
   }, [gameDate, coordinates]);
 
@@ -64,23 +76,28 @@ const SlidingPanel = ({ yearArr }) => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
     const courtImage = imageRef.current;
-    const markerImage = markerRef.current;
 
     const drawImage = () => {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       ctx.drawImage(courtImage, 0, 0, canvas.width, canvas.height);
 
-      // Loop through filtered coordinates array and draw each marker image
+      // Load marker images
+      const markerGreenImage = new Image();
+      const markerRedImage = new Image();
+      markerGreenImage.src = markerGreen;
+      markerRedImage.src = markerRed;
+
+      // Draw markers
       filteredCoordinates.forEach(coord => {
-        ctx.drawImage(markerImage, coord.left - 12.5, coord.top - 12.5, 10, 10); // Adjust the coordinates to center the marker
+        const markerImage = coord.color === 'green' ? markerGreenImage : markerRedImage;
+        ctx.drawImage(markerImage, coord.left - 9, coord.top - 9, 18, 18); // Adjust the size of the marker
       });
     };
 
-    if (courtImage.complete && markerImage.complete) {
+    if (courtImage.complete) {
       drawImage();
     } else {
       courtImage.onload = drawImage;
-      markerImage.onload = drawImage;
     }
   }, [filteredCoordinates]);
 
@@ -92,7 +109,7 @@ const SlidingPanel = ({ yearArr }) => {
           <h3>{playerName}</h3>
           <div><Dropdown options={playerArr} onSelect={setPlayerName} defaultSelection={playerArr[0]} label='Select a player:' className='selectPlayer' /></div>
           <div>
-            <Dropdown options={yearArr} onSelect={handleYearSelect} defaultSelection={yearArr[yearArr.length - 1]} label='Select season ending year:' className='selectSeason' /> 
+            <Dropdown options={yearArr} onSelect={handleYearSelect} defaultSelection={yearArr[yearArr.length - 1].toString()} label='Select season ending year:' className='selectSeason' /> 
           </div>
           <div>
               {uniqueDates.length > 0 && (
@@ -102,8 +119,10 @@ const SlidingPanel = ({ yearArr }) => {
         </div>
         <div className="canvas-container">
           <img ref={imageRef} src={court} className="canvas-background" alt="Basketball Court" />
-          <img ref={markerRef} src={marker} className="marker" alt="Marker" style={{ display: 'none' }} /> {/* Hidden marker image for drawing */}
           <canvas ref={canvasRef} width={500} height={500}></canvas>
+          <div className="margin-top-small">Total Shots: {totalShots}</div>
+          <div>Total Made Shots: {totalMadeShots}</div>
+          <div>Shooting Percentage: {totalMadeShots/totalShots * 100}%</div>
         </div>
       </div>
     </div>
