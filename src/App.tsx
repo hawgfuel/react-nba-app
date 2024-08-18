57dbf14f423caf6d1892cc0940d8d2921ece2297
Alan Werstler <hawgfuel@gmail.com>
2024-08-18 11:24:56 -0700

    converted files to tsx and clean up file

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NBA } from './pages/nba/nba';
+
+function App() {
+  return (
+    <div className="App">
+      <NBA />
+    </div>
+  );
+}
+
+export default App;
